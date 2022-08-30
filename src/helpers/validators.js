7826e026073239f3d8ca3d76e3dd4ac855e92e96
shulgin01 <shulgin01@inbox.ru>
2022-08-30 13:06:40 +0300

    test

--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -12,24 +12,77 @@
  *
  * Если какие либо функции написаны руками (без использования библиотек) это не является ошибкой
  */
+import {allPass, compose, count, equals, lte, not, prop, values} from "ramda";
+
+const getStar = prop('star')
+const getSquare = prop('square')
+const getTriangle = prop('triangle')
+const getCircle = prop('circle')
+
+const isGreen = equals('green')
+const isWhite = equals('white')
+const isRed = equals('red')
+const isOrange = equals('orange')
+const isBlue = equals('blue')
+
+const isGreenSquare = compose(
+    isGreen,
+    getSquare
+)
+const isRedStar = compose(
+    isRed,
+    getStar
+)
+const isWhiteTriangle = compose(
+    isWhite,
+    getTriangle
+)
+const isWhiteCircle = compose(
+    isWhite,
+    getCircle
+)
+const isNotRedStar = compose(
+    not,
+    isRedStar
+)
+const isNotWhiteStar = compose(
+    not,
+    isWhite,
+    getStar
+)
+
+const countRedFigures = compose(
+    count(isRed),
+    values
+)
+const countBlueFigures = compose(
+    count(isBlue),
+    values
+)
 
 // 1. Красная звезда, зеленый квадрат, все остальные белые.
-export const validateFieldN1 = ({star, square, triangle, circle}) => {
-    if (triangle !== 'white' || circle !== 'white') {
-        return false;
-    }
-
-    return star === 'red' && square === 'green';
-};
+export const validateFieldN1 = allPass([
+    isRedStar,
+    isGreenSquare,
+    isWhiteTriangle,
+    isWhiteCircle
+])
 
 // 2. Как минимум две фигуры зеленые.
-export const validateFieldN2 = () => false;
+export const validateFieldN2 = compose(
+    lte(2),
+    count(isGreen),
+    values
+)
 
 // 3. Количество красных фигур равно кол-ву синих.
-export const validateFieldN3 = () => false;
+export const validateFieldN3 = event => equals(countRedFigures(event), countBlueFigures(event))
 
 // 4. Синий круг, красная звезда, оранжевый квадрат треугольник любого цвета
-export const validateFieldN4 = () => false;
+export const validateFieldN4 = (c) => {
+    console.log(c)
+    return false
+}
 
 // 5. Три фигуры одного любого цвета кроме белого (четыре фигуры одного цвета – это тоже true).
 export const validateFieldN5 = () => false;
@@ -38,13 +91,21 @@ export const validateFieldN5 = () => false;
 export const validateFieldN6 = () => false;
 
 // 7. Все фигуры оранжевые.
-export const validateFieldN7 = () => false;
+export const validateFieldN7 = compose(
+    equals(4),
+    count(isOrange),
+    values
+);
 
 // 8. Не красная и не белая звезда, остальные – любого цвета.
-export const validateFieldN8 = () => false;
+export const validateFieldN8 = allPass([isNotRedStar, isNotWhiteStar]);
 
 // 9. Все фигуры зеленые.
-export const validateFieldN9 = () => false;
+export const validateFieldN9 = compose(
+    equals(4),
+    count(isGreen),
+    values
+);
 
 // 10. Треугольник и квадрат одного цвета (не белого), остальные – любого цвета
 export const validateFieldN10 = () => false;
