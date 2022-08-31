43d09d30c781e4a0b75cfa78d70df40cdcf4ceaa
shulgin01 <shulgin01@inbox.ru>
2022-08-31 19:59:22 +0300

    done

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -1,51 +1,98 @@
-/**
- * @file Домашка по FP ч. 2
- *
- * Подсказки:
- * Метод get у инстанса Api – каррированый
- * GET / https://animals.tech/{id}
- *
- * GET / https://api.tech/numbers/base
- * params:
- * – number [Int] – число
- * – from [Int] – из какой системы счисления
- * – to [Int] – в какую систему счисления
- *
- * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
- * Ответ будет приходить в поле {result}
- */
- import Api from '../tools/api';
+import Api from '../tools/api';
+import {
+    allPass,
+    compose,
+    ifElse,
+    tap,
+    length,
+    partial,
+    gt,
+    lt,
+    andThen,
+    __,
+    test,
+    assoc,
+    prop,
+    mathMod,
+    concat, otherwise
+} from "ramda";
 
- const api = new Api();
+const api = new Api();
 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
+const API_NUMBERS_URL = 'https://api.tech/numbers/base'
+const API_ANIMALS_URL = 'https://animals.tech/'
 
- const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
+const getApiResult = compose(String, prop('result'))
+const thenGetApiResult = andThen(getApiResult)
 
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
+const lengthGreaterThenTwo = compose(
+    gt(__, 2),
+    length
+)
+const lengthLowerThenTen = compose(
+    lt(__, 10),
+    length
+)
+const onlyNumbers = test(/^[0-9]+\.?[0-9]+$/)
+const validate = allPass([lengthGreaterThenTwo, lengthLowerThenTen, onlyNumbers])
 
-     wait(2500).then(() => {
-         writeLog('SecondLog')
+const stringToNumber = compose(
+    Math.round,
+    Number
+)
 
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
+const getLength = andThen(length)
 
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
- }
+const getSquare = andThen(num => num ** 2)
 
- export default processSequence;
+const getModForThree = andThen(compose(
+    String,
+    mathMod(__, 3)
+))
+
+const getBinaryBase = compose(
+    api.get(API_NUMBERS_URL),
+    assoc('number', __, {from: 10, to: 2})
+)
+const getAnimals = andThen(compose(
+    api.get(__, {}),
+    concat(API_ANIMALS_URL)
+))
+
+
+const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
+    const tapLog = tap(writeLog)
+    const thenTapLog = andThen(tapLog)
+
+    const thenHandleSuccess = andThen(handleSuccess)
+
+    const otherwiseHandleError = otherwise(handleError)
+    const handleValidationError = partial(handleError, ['ValidationError'])
+
+    const doAndLog = (x) => compose(
+        thenTapLog,
+        x
+    )
+
+    const sequenceComposition = compose(
+        otherwiseHandleError,
+        thenHandleSuccess,
+        thenGetApiResult,
+        getAnimals,
+        doAndLog(getModForThree),
+        doAndLog(getSquare),
+        doAndLog(getLength),
+        thenTapLog,
+        thenGetApiResult,
+        getBinaryBase,
+        tapLog,
+        stringToNumber
+    )
+
+    compose(
+        ifElse(validate, sequenceComposition, handleValidationError),
+        tapLog
+    )(value)
+}
+
+export default processSequence;
--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -1,18 +1,4 @@
-/**
- * @file Домашка по FP ч. 1
- *
- * Основная задача — написать самому, или найти в FP библиотеках функции anyPass/allPass
- * Эти функции/их аналоги есть и в ramda и в lodash
- *
- * allPass — принимает массив функций-предикатов, и возвращает функцию-предикат, которая
- * вернет true для заданного списка аргументов, если каждый из предоставленных предикатов
- * удовлетворяет этим аргументам (возвращает true)
- *
- * anyPass — то же самое, только удовлетворять значению может единственная функция-предикат из массива.
- *
- * Если какие либо функции написаны руками (без использования библиотек) это не является ошибкой
- */
-import {allPass, compose, count, equals, lte, not, prop, values} from "ramda";
+import {allPass, anyPass, compose, count, curry, equals, lte, not, prop, values} from "ramda";
 
 const getStar = prop('star')
 const getSquare = prop('square')
@@ -25,87 +11,82 @@ const isRed = equals('red')
 const isOrange = equals('orange')
 const isBlue = equals('blue')
 
-const isGreenSquare = compose(
-    isGreen,
-    getSquare
-)
-const isRedStar = compose(
-    isRed,
-    getStar
-)
-const isWhiteTriangle = compose(
-    isWhite,
-    getTriangle
-)
-const isWhiteCircle = compose(
-    isWhite,
-    getCircle
-)
-const isNotRedStar = compose(
-    not,
-    isRedStar
-)
-const isNotWhiteStar = compose(
-    not,
-    isWhite,
-    getStar
-)
+const isNotWhite = compose(not, isWhite);
+const isNotRed = compose(not, isRed);
 
-const countRedFigures = compose(
-    count(isRed),
+const areTwoValuesSame = (values) => equals(...values);
+
+const getTriangleAndSquare = (event) => [getTriangle(event), getSquare(event)];
+
+const isColorFigure = (getFigure, isColor) => compose(
+    isColor,
+    getFigure
+)
+const curriedIsColorFigure = curry(isColorFigure)
+const isColorStar = curriedIsColorFigure(getStar)
+
+const countColor = (color) => compose(
+    count(color),
     values
 )
-const countBlueFigures = compose(
-    count(isBlue),
-    values
+const hasCountColor = (count, color) => compose(
+    lte(count),
+    countColor(color)
 )
-
+const equalsCountColor = (count, color) => compose(
+    equals(count),
+    countColor(color)
+)
 // 1. Красная звезда, зеленый квадрат, все остальные белые.
 export const validateFieldN1 = allPass([
-    isRedStar,
-    isGreenSquare,
-    isWhiteTriangle,
-    isWhiteCircle
+    isColorFigure(getStar, isRed),
+    isColorFigure(getSquare, isGreen),
+    isColorFigure(getTriangle, isWhite),
+    isColorFigure(getCircle, isWhite)
 ])
 
 // 2. Как минимум две фигуры зеленые.
-export const validateFieldN2 = compose(
-    lte(2),
-    count(isGreen),
-    values
-)
+export const validateFieldN2 = hasCountColor(2, isGreen)
 
 // 3. Количество красных фигур равно кол-ву синих.
-export const validateFieldN3 = event => equals(countRedFigures(event), countBlueFigures(event))
+export const validateFieldN3 = event => equals(countColor(isRed)(event), countColor(isBlue)(event))
 
 // 4. Синий круг, красная звезда, оранжевый квадрат треугольник любого цвета
-export const validateFieldN4 = (c) => {
-    console.log(c)
-    return false
-}
+export const validateFieldN4 = allPass([
+    isColorFigure(getCircle, isBlue),
+    isColorStar(isRed),
+    isColorFigure(getSquare, isOrange)
+])
 
 // 5. Три фигуры одного любого цвета кроме белого (четыре фигуры одного цвета – это тоже true).
-export const validateFieldN5 = () => false;
+export const validateFieldN5 = anyPass([
+    hasCountColor(3, isGreen),
+    hasCountColor(3, isOrange),
+    hasCountColor(3, isRed),
+    hasCountColor(3, isBlue)
+])
 
 // 6. Ровно две зеленые фигуры (одна из зелёных – это треугольник), плюс одна красная. Четвёртая оставшаяся любого доступного цвета, но не нарушающая первые два условия
-export const validateFieldN6 = () => false;
+export const validateFieldN6 = allPass([
+    isColorFigure(getTriangle, isGreen),
+    equalsCountColor(2, isGreen),
+    equalsCountColor(1, isRed)
+])
 
 // 7. Все фигуры оранжевые.
-export const validateFieldN7 = compose(
-    equals(4),
-    count(isOrange),
-    values
-);
+export const validateFieldN7 = equalsCountColor(4, isOrange)
 
 // 8. Не красная и не белая звезда, остальные – любого цвета.
-export const validateFieldN8 = allPass([isNotRedStar, isNotWhiteStar]);
-
-// 9. Все фигуры зеленые.
-export const validateFieldN9 = compose(
-    equals(4),
-    count(isGreen),
-    values
+export const validateFieldN8 = compose(
+    allPass([isNotRed, isNotWhite]),
+    getStar
 );
 
+// 9. Все фигуры зеленые.
+export const validateFieldN9 = equalsCountColor(4, isGreen)
+
 // 10. Треугольник и квадрат одного цвета (не белого), остальные – любого цвета
-export const validateFieldN10 = () => false;
+export const validateFieldN10 = compose(
+    allPass([equalsCountColor(0, isWhite), areTwoValuesSame]),
+    getTriangleAndSquare
+);
